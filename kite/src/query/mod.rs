c781e167c193dfa91938019e9c494d1825841002
Karl Hobley <karl@kaed.uk>
2017-07-24 23:01:59 +0100

    Document queries

--- kite/src/query/mod.rs
+++ kite/src/query/mod.rs
@@ -9,33 +9,67 @@ use query::term_scorer::TermScorer;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum Query {
+    /// Matches all documents, assigning the specified score to each one
     All {
+        /// The score to assign to each document
         score: f32,
     },
+    
+    /// Matches nothing
     None,
+    
+    /// Matches documents that contain the specified term in the specified field
     Term {
+        /// The field being searched
         field: FieldRef,
+        
+        /// The term to search for
         term: Term,
+
+        /// The method of scoring each match
         scorer: TermScorer,
     },
+    
+    /// Matches documents by a multi term selector
+    /// Used for prefix, fuzzy and regex queries
     MultiTerm {
+        /// The field being searched
         field: FieldRef,
+        
+        /// The term selector to use. All terms that match this selector will be searched
         term_selector: MultiTermSelector,
+        
+        /// The method of scoring each match.
         scorer: TermScorer,
     },
+    
+    /// Joins two queries with an AND operator
+    /// This intersects the results of the queries. The scores are combined by average
     Conjunction {
         queries: Vec<Query>,
     },
+    
+    /// Joins two queries with an OR operator
+    /// This unites the results of the queries. The scores are combined by average
     Disjunction {
         queries: Vec<Query>,
     },
+    
+    /// Joins two queries with an OR operator
+    /// This unites the results of the queries.
+    /// Unlike a regular Disjunction query, this takes the highest score of each query for a particular match
     DisjunctionMax {
         queries: Vec<Query>,
     },
+    
+    /// Removes documents that do not match the "filter" query from the results
+    /// Basically the same as a Conjunction query except that the "filter" query does not affect the score
     Filter {
         query: Box<Query>,
         filter: Box<Query>
     },
+    
+    /// Removes documents that match the "exclude" query from the results
     Exclude {
         query: Box<Query>,
         exclude: Box<Query>
@@ -44,12 +78,14 @@ pub enum Query {
 
 
 impl Query {
+    /// Creates a new All query
     pub fn all() -> Query {
         Query::All {
             score: 1.0f32,
         }
     }
 
+    /// Creates a new Term query
     pub fn term(field: FieldRef, term: Term) -> Query {
         Query::Term {
             field: field,
@@ -58,6 +94,8 @@ impl Query {
         }
     }
 
+    /// Filters the query by another query
+    /// Only documents that match the other query will remain in the results but the other query will not affect the score
     pub fn filter(self, filter: Query) -> Query {
         Query::Filter {
             query: Box::new(self),
@@ -65,6 +103,7 @@ impl Query {
         }
     }
 
+    /// Filters the query to exclude documents that match the other query
     pub fn exclude(self, exclude: Query) -> Query {
         Query::Exclude {
             query: Box::new(self),
@@ -73,6 +112,7 @@ impl Query {
     }
 
     #[inline]
+    /// Multiplies the score of documents that match the query by the specified "boost" value
     pub fn boost(mut self, boost: f32) -> Query {
         self.add_boost(boost);
         self
